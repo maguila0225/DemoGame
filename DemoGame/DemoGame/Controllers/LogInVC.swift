9eebd867842c9ab08bd4c38f75b8aede99112e7d
Miguel Aguila <maguila.apple@gmail.com>
2022-01-16 04:08:48 +0800

    completed: 1. Firestore database used for log in credentials

--- DemoGame/DemoGame/Controllers/LogInVC.swift
+++ DemoGame/DemoGame/Controllers/LogInVC.swift
@@ -8,6 +8,7 @@
 import UIKit
 import MaterialComponents.MaterialTextControls_OutlinedTextFields
 import MaterialComponents.MaterialButtons
+import Firebase
 // MARK: - Global Log functions
 func GlobalLog_Load (vc_Log: String){
     NSLog("View Controller \(vc_Log) Loaded")
@@ -28,6 +29,8 @@ class LogInVC: UIViewController {
     @IBOutlet weak var usernameTextField: MDCOutlinedTextField!
     @IBOutlet weak var passwordTextField: MDCOutlinedTextField!
     
+    let firestoreDatabase = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -46,12 +49,29 @@ class LogInVC: UIViewController {
     }
     @IBAction func logInAttempt(_ sender: Any) {
         NSLog("Log in Attempted")
+        guard usernameTextField.text?.isEmpty == false
+                && passwordTextField.text?.isEmpty == false
+        else{
+            NSLog("Empty username/password")
+            return
+        }
+        let documentReference = firestoreDatabase.document("playerDatabase/\(usernameTextField.text ?? "")")
+        documentReference.getDocument{ snapshot, error in
+            guard let data = snapshot?.data(), error == nil else{
+                NSLog("Invalid User")
+                return
+            }
+            guard data[self.usernameTextField.text!] as! String == self.passwordTextField.text! else{
+                NSLog("Incorrect Password")
+                return
+            }
+        NSLog("Successful Log In")
+        }
     }
     @IBAction func registerButton(_ sender: Any) {
         screenTransition(vcIdentifier: "RegistrationVC",isFullScreen: true)
     }
 }
-
 extension LogInVC{
     public func screenTransition (vcIdentifier: String, isFullScreen: Bool) {
         let sb = UIStoryboard(name: "Main", bundle: nil)
@@ -61,5 +81,6 @@ extension LogInVC{
         }
         self.present(vc, animated: true, completion: nil)
     }
+    
 }
 
--- DemoGame/DemoGame/Controllers/RegistrationVC.swift
+++ DemoGame/DemoGame/Controllers/RegistrationVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import MaterialComponents.MDCOutlinedTextField
+import FirebaseFirestore
 
 class RegistrationVC: UIViewController {
 
@@ -15,8 +16,11 @@ class RegistrationVC: UIViewController {
     @IBOutlet weak var passwordRegistration: MDCOutlinedTextField!
     @IBOutlet weak var passwordConfirmation: MDCOutlinedTextField!
     
+    let firestoreDatabase = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         usernameRegistration.label.text = "Username"
         usernameRegistration.leadingAssistiveLabel.text = ""
         
@@ -42,5 +46,25 @@ class RegistrationVC: UIViewController {
     @IBAction func exitButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-   
+    @IBAction func registerUser(_ sender: Any) {
+        guard usernameRegistration.text?.isEmpty == false
+                && passwordConfirmation.text?.isEmpty == false
+                && passwordRegistration.text == passwordConfirmation.text
+        else{
+            if usernameRegistration.text?.isEmpty == true{
+                usernameRegistration.leadingAssistiveLabel.text = "Invalid Username"
+            }
+            if passwordConfirmation.text != passwordRegistration.text{
+                passwordRegistration.leadingAssistiveLabel.text = "Mismatch Password"
+            }
+                    return
+            }
+    
+        registerUser(username: usernameRegistration.text!, password: passwordConfirmation.text!)
+    }
+    
+    func registerUser(username: String, password: String){
+        let documentReference = firestoreDatabase.document("playerDatabase/\(username)")
+        documentReference.setData([username:password])
+    }
 }
