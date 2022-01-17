0eb1fc61acd9f78daf765ac7b02b5639bd55f84b
Miguel Aguila <maguila.apple@gmail.com>
2022-01-17 11:13:35 +0800

    completed: 1. single player screen layout final

--- DemoGame/DemoGame/Controllers/SinglePlayerVC.swift
+++ DemoGame/DemoGame/Controllers/SinglePlayerVC.swift
@@ -10,22 +10,33 @@ import MaterialComponents.MaterialAppBar
 
 // MARK: - SinglePlayerVC
 class SinglePlayerVC: UIViewController {
-// MARK: - SinglePlayerVC variable declaration
+// MARK: - SinglePlayerVC Declaration
 
+//MARK: - Screen Output Declaration
     @IBOutlet weak var p1SelectedImage: UIImageView!
     @IBOutlet weak var p2SelectedImage: UIImageView!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var playerName: UILabel!
+    @IBOutlet weak var playerName_Score: UILabel!
+    @IBOutlet weak var playerScore: UILabel!
+    @IBOutlet weak var botScore: UILabel!
+    
+// MARK: - User Input Declaration
     @IBOutlet weak var rockButton: UIImageView!
     @IBOutlet weak var paperButton: UIImageView!
     @IBOutlet weak var ScissorsButton: UIImageView!
     @IBOutlet weak var lizardButton: UIImageView!
     @IBOutlet weak var spockButton: UIImageView!
+    @IBOutlet weak var fightButton: MDCButton!
+
+// MARK: -  Variables/Constants
     let vcIdentifier = "SinglePlayerVC"
     
 // MARK: - SinglePlayerVC life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         GlobalLog_Load(vc_Log: vcIdentifier)
+        
         p1SelectedImage.image = UIImage(named: "Rock.png")
         p2SelectedImage.image = UIImage(named: "Spock.png")
         rockButton.image = UIImage(named: "Rock.png")
@@ -34,9 +45,11 @@ class SinglePlayerVC: UIViewController {
         lizardButton.image = UIImage(named: "Lizard.png")
         spockButton.image = UIImage(named: "Spock.png")
         
+        
     }
     override func  viewWillAppear(_ animated: Bool) {
         GlobalLog_Display(vc_Log: vcIdentifier)
+    
 
     }
     
